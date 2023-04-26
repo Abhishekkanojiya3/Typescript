41fd39ccf1889890e249cf00e345c10969589f36
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-04-26 23:53:17 +0530

    typescript with nodejs

---
+++ NodeJS_With_Typescript/app.ts
@@ -0,0 +1,9 @@
+const express = require('express')
+import bodyParser from "body-parser";
+const app = express();
+import todosRoutes from "./routes/todos";
+
+app.use(bodyParser.json())
+app.use(todosRoutes)
+
+app.listen(3000)
